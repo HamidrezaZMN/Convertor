98e74b8f67c2db9408a0493fc3438576f7b98f32
Hamidreza Zamanian <hamid80zamanian@gmail.com>
2020-07-12 18:00:51 +0430

    #1

---
+++ others/convertor.py
@@ -0,0 +1,53 @@
+def main():
+    root = tk.Tk()
+    root.withdraw()
+
+    dir = ''
+    files = filedialog.askopenfilenames()
+    for file in files:
+        paths = [i.strip() for i in file.split('/')]
+        first_dir = '/'.join(paths[0:-1]) + '/'
+        dir = first_dir
+        last_one = paths[-1]
+        mp4 = first_dir
+        mp3 = first_dir
+
+        if last_one[-4:]=='.mp4':
+            mp4 += last_one
+            mp3 += last_one[:-1] + '3'
+        else:
+            mp4 += last_one + '.mp4'
+            mp3 += last_one + '.mp3'
+        
+        try:
+            video = VideoFileClip(mp4)
+            video.audio.write_audiofile(mp3)
+            ctypes.windll.user32.MessageBoxW(
+                0, 
+                f'"{paths[-1]}" is converted', 
+                'Result', 
+                1
+            )
+        except:
+            ctypes.windll.user32.MessageBoxW(
+                0, 
+                'An Error Occured!\n'+ 
+                f'can\'t convert "{paths[-1]}"', 
+                'Error', 
+                1
+            )
+    webbrowser.open(dir)
+
+import os, sys, webbrowser, ctypes
+
+try:
+    import tkinter as tk
+    from tkinter import filedialog
+except:
+    import Tinter as tk
+    from Tkinter import filedialog
+
+import easygui
+from moviepy.editor import *
+
+main()
